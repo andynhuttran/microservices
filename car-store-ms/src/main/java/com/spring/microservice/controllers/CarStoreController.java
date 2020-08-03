9044a3b98f88d302e39db31114abd71e99ae0240
colenhut <tranthenhut@gmail.com>
2020-08-03 07:08:40 +0700

    add more API in Car Store

--- car-store-ms/src/main/java/com/spring/microservice/controllers/CarStoreController.java
+++ car-store-ms/src/main/java/com/spring/microservice/controllers/CarStoreController.java
@@ -1,9 +1,12 @@
 package com.spring.microservice.controllers;
 
 import java.util.List;
+import java.util.function.Predicate;import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.microservice.object.Car;
@@ -46,6 +49,28 @@ public class CarStoreController {
 		});
 		
 		return cars;
+	}
+	
+	
+	@GetMapping("/cars/from/{from}/to/{to}")
+	public List<Car> getCarsByPrice(@PathVariable("from") double from, @PathVariable("to") double to) {
+		List<Car> cars = carDAO.getAllCar();
+		cars.forEach(c -> {
+			c.setDatabaseConfigurationValue(databaseConfigurationValue);
+			c.setHost(databaseConfigurationValue.getHost());
+		});
+		
+		List<Car> list = cars.stream().filter(new Predicate<Car>() {
+			@Override
+			public boolean test(Car c) {
+				if (from <= c.getPrice() && c.getPrice() <= to) {
+					return true;
+				}
+				return false;
+			}
+		}).collect(Collectors.toList());
+
+		return list;
 	}
 	
 	
